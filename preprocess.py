b6ebb38fc676de2425c3dbf26fa7076dad610b00
Minsheng Liu <lambda@liu.ms>
2019-04-20 13:44:33 -0700

    Add VGG11-BN.

--- preprocess.py
+++ preprocess.py
@@ -7,46 +7,56 @@ import PIL
 
 import torch
 
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms, utils
+from torch.utils.data import Dataset
+from torchvision import transforms
 
 index  = simplejson.load(open('./data/train.json'))
-labels = torch.LongTensor([int(x['breedID']) for x in index])
+# shift to make it zero-indexed
+labels = torch.LongTensor([int(x['breedID']) for x in index]) - 1
 
-def load_training_images():
-  IMG = './data/images/train.torch'
+def load_images(phase, index_range):
+  IMG = './data/images/' + phase + '.torch'
   if os.path.isfile(IMG):
     return torch.load(IMG)
   else:
     images = []
-    for i in range(len(index)):
+    for i in index_range:
       if i % 100 == 0:
         print('%d images loaded.' % i)
-      image_name = os.path.join('./data/images/train', str(i) + '.jpg')
+      image_name = os.path.join('./data/images/', phase, str(i) + '.jpg')
       image = io.imread(image_name)
       images.append(torch.tensor(image))
     torch.save(images, IMG)
     return images
 
-images = load_training_images()
+test_images  = load_images('test', range(3680, 7349))
+train_images = load_images('train', range(len(index)))
 
 def toPIL(tensor):
   return PIL.Image.fromarray(tensor.numpy())
 
 class TrainingDataset(Dataset):
   def __init__(self, input_size):
-    self.labels = labels
-    transform = transforms.Compose([
+    self.transform = transforms.Compose([
       toPIL,
       transforms.RandomResizedCrop(input_size),
       transforms.RandomHorizontalFlip(),
       transforms.ToTensor(),
       transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
-    self.images = [transform(x) for x in images]
 
   def __len__(self):
-    return len(self.images)
+    return len(train_images)
 
-  def __getitem(self, idx):
-    return self.images[idx], self.labels[idx]
+  def __getitem__(self, idx):
+    return self.transform(train_images[idx]), labels[idx]
+
+def transform_test_images(input_size):
+  f = transforms.Compose([
+    toPIL,
+    transforms.Resize(input_size),
+    transforms.CenterCrop(input_size),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+  ])
+  return [f(x) for x in test_images]
---
+++ vgg.py
@@ -0,0 +1,115 @@
+import time
+import copy
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from torch.utils.data import DataLoader, random_split
+from torchvision.models import vgg11_bn
+
+from preprocess import TrainingDataset, labels
+
+input_size = 224
+batch_size = 16
+num_epochs = 25
+
+num_classes = torch.unique(labels).shape[0]
+
+dataset = TrainingDataset(input_size)
+
+len_train_set = int(len(dataset) * 0.7)
+len_val_set   = len(dataset) - len_train_set
+
+train_set, val_set = random_split(dataset, [len_train_set, len_val_set])
+
+datasets = {
+  'train': train_set,
+  'val': val_set
+}
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+def train(use_pretrained = False):
+  loaders = {
+    x: DataLoader(datasets[x],
+      batch_size = batch_size, shuffle = True, num_workers = 4)
+    for x in ['train', 'val']
+  }
+
+  model = vgg11_bn()
+  model.classifier[6] = nn.Linear(4096, num_classes)
+  model = model.to(device)
+
+  optimizer = optim.Adam(model.parameters(), 1e-4)
+
+  criterion = nn.CrossEntropyLoss()
+
+  since = time.time()
+
+  val_acc_history = []
+
+  best_model_wts = copy.deepcopy(model.state_dict())
+  best_acc = 0.0
+
+  for epoch in range(num_epochs):
+    print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+    print('-' * 10)
+
+    # Each epoch has a training and validation phase
+    for phase in ['train', 'val']:
+      if phase == 'train':
+          model.train()  # Set model to training mode
+      else:
+          model.eval()   # Set model to evaluate mode
+
+      running_loss = 0.0
+      running_corrects = 0
+
+      # Iterate over data.
+      for inputs, labels in loaders[phase]:
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward
+        # track history if only in train
+        with torch.set_grad_enabled(phase == 'train'):
+          outputs = model(inputs)
+          print(outputs.shape, labels)
+          loss = criterion(outputs, labels)
+
+          _, preds = torch.max(outputs, 1)
+
+          # backward + optimize only if in training phase
+          if phase == 'train':
+              loss.backward()
+              optimizer.step()
+
+      # statistics
+      running_loss += loss.item() * inputs.size(0)
+      running_corrects += torch.sum(preds == labels.data)
+
+    epoch_loss = running_loss / len(loaders[phase].dataset)
+    epoch_acc = running_corrects.double() / len(loaders[phase].dataset)
+
+  print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+
+  # deep copy the model
+  if phase == 'val' and epoch_acc > best_acc:
+      best_acc = epoch_acc
+      best_model_wts = copy.deepcopy(model.state_dict())
+  if phase == 'val':
+      val_acc_history.append(epoch_acc)
+
+      print()
+
+  time_elapsed = time.time() - since
+  print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+  print('Best val Acc: {:4f}'.format(best_acc))
+
+  # load best model weights
+  model.load_state_dict(best_model_wts)
+  return model, val_acc_history
